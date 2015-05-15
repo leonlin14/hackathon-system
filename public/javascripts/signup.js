561bceccaa9e928632ba5a5fcf02e5f2187048ed
leonlin14 <leonlin14@gmail.com>
2015-05-16 03:19:25 +0800

    update alert

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -5,16 +5,17 @@ app.controller('SignUpController', function($rootScope, $scope, $http) {
         $http.get('/checkmember/' + idno)
             .success(function(resp) {	
                 console.log(resp);
-                $scope.data = resp;
+                $scope.data = 'success';
 
 				$http.get('/cardreader')
 					.success(function(resp) {
 						console.log(resp);
 						var token = resp.token;
 
-						$http.post('http://123.23.23.23/opencard', { email: $scope.email, token: token, cardno: idno })
+						$http.post('http://123.23.23.23/opencard', { 'email': $scope.email, 'token': token, 'cardno': idno })
 							.success(function(resp) {
 								// Show message
+								$scope.data = 'savedCard';
 							});
 						
 					});
