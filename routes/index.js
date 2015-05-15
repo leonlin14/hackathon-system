0c6f28f5356786c9cf7c76adf4115212ceaa6743
Fred Chien <cfsghost@gmail.com>
2015-05-16 03:02:16 +0800

    add APIs to check member and get card number

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,75 @@
 var express = require('express');
+//var cards = require('../data/cards.json');
+var members = require('../data/members.json');
 var router = express.Router();
+
+// Event dispatcher
+var events = require('events');
+var eventEngine = new events.EventEmitter();
+
+// Card reader
+var pcsc = require('pcsclite');
+var pcsc = pcsc();
+pcsc.on('reader', function(reader) {
+	console.log('Detected ' + reader.name);
+
+	reader.on('status', function(status) {
+		var changes = this.state ^ status.state;
+		if (!changes)
+			return;
+
+		// Card removed
+		if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+			reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+				console.log('Card removed');
+
+			});
+			return;
+		}
+
+		// Detected card
+		if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+			console.log('Detected card');
+			reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+				if (err) {
+					console.err(err);
+					return;
+				}
+
+				// Getting serial number
+				reader.transmit(new Buffer([0xFF, 0xCA, 0x00, 0x00, 0x00]), 40, protocol, function(err, data) {
+					eventEngine.emit('passed', data.toString('hex'));
+				});
+			});
+		}
+	});
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/checkmember/:memberno', function(req, res) {
+	for (var index in members) {
+		var member = members[index];
+
+		if (member.regno == req.params.memberno) {
+			res.json(member);
+			break;
+		}
+	}
+
+	res.status(404).end();
+});
+
+router.get('/cardreader', function(req, res) {
+
+	eventEngine.once('passed', function(token) {
+		res.json({
+			token: token
+		});
+	});
 });
 
 module.exports = router;
