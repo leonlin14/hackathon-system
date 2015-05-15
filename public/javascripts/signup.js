3d6767d8114eb23f02318be54fb2576c6a87dcb5
Fred Chien <cfsghost@gmail.com>
2015-05-16 03:11:56 +0800

    support to read card number

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,10 +2,22 @@ var app = angular.module('signup', []);
 
 app.controller('SignUpController', function($rootScope, $scope, $http) {
     $scope.submit = function(idno) {
-        $http.get('/signup', {"idno": idno})
+        $http.get('/checkmember/' + idno)
             .success(function(resp) {	
                 console.log(resp);
                 $scope.data = resp;
+
+				$http.get('/cardreader')
+					.success(function(resp) {
+						console.log(resp);
+						var token = resp.token;
+
+						$http.post('http://123.23.23.23/opencard', { email: $scope.email, token: token, cardno: idno })
+							.success(function(resp) {
+								// Show message
+							});
+						
+					});
         	})
         	.error(function(resp) {
         		console.log('error');
